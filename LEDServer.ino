ea9c98908ddc1c5bf8b165226c1efab36af9a867
Chandler Swift <chandler@chandlerswift.com>
2017-01-13 10:17:23 -0600

    Cumulative Updates to date

--- LEDServer.ino
+++ LEDServer.ino
@@ -1,9 +1,22 @@
 #include <ESP8266WiFi.h>
-#include <DNSServer.h> //Local DNS Server used for redirecting all requests to the configuration portal
+#include <DNSServer.h> // Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>
+//#include <ESP8266HTTPClient.h> // TODO Verify this works without
 #include <WiFiManager.h> //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include "FS.h"
 #include "light.h"
+
+/* 
+ *  GPIO Assignments:
+ *  0:  Enable/Disable web control switch
+ *  2:  Built-in status LED
+ *  4:  Door switch sensor
+ *  5:  Manual control button
+ *  12: LED green channel
+ *  13: LED blue channel
+ *  14: LED red channel
+ *  15: Main light
+ */
 
 ESP8266WebServer server(80);
 const char* www_username = "user";
@@ -13,45 +26,75 @@ bool authRequired = false;
 
 const int numLights = 5;
 Light* lights[numLights];
-Light *r, *g, *b;
 
-int interruptPin = 4;
+int enable_switch_pin = 0;
+
+int door_pin = 4;
 bool door_control = false;
-void setLightState() {
+void setLightStateOnDoorAction() {
   if (door_control)
-    lights[1]->set(digitalRead(interruptPin) == LOW);
+    lights[1]->set(digitalRead(door_pin) == LOW);
 }
 
-void setup(void){
+int button_pin = 5;
+void toggleLightOnButtonPress() {
+  /*
+   * If the main light is on, turns off
+   * If the main light is not on but the LEDs are, turns off LEDs
+   * If no lights are on, turns on main light
+   */
+  if (lights[1]->get() > 0)
+    lights[1]->set(0);
+  else if (lights[2] > 0 || lights[3] > 0 || lights[4] > 0) {
+    lights[2]->set(0);
+    lights[3]->set(0);
+    lights[4]->set(0);
+  } else {
+    lights[1]->set(1);
+  }
+}
+
+void setup(void) {
+  // Set up PWM
   analogWriteRange(255);
-  analogWriteFreq(59);
+  analogWriteFreq(200);
 
-  lights[0] = new Light(2, "Built-in Status LED", true, true); // built-in led
-  lights[1] = new Light(15, "Main Light");              // Relay light
-  lights[2] = r = new Light(14, "RGB LED Strip: Red Channel", true);   // LED strip red channel
-  lights[3] = g = new Light(12, "RGB LED Strip: Green Channel", true);   // LED strip green channel
-  lights[4] = b = new Light(13, "RGB LED Strip: Blue Channel", true);   // LED strip blue channel
+  lights[0] = new Light(2, "Built-in Status LED", true, true);       // built-in led
+  lights[1] = new Light(15, "Main Light");                           // Relay light
+  lights[2] = new Light(14, "RGB LED Strip: Red Channel", true);     // LED strip red channel
+  lights[3] = new Light(12, "RGB LED Strip: Green Channel", true);   // LED strip green channel
+  lights[4] = new Light(13, "RGB LED Strip: Blue Channel", true);    // LED strip blue channel
 
+  // Start Filesystem
   SPIFFS.begin();
+
+  // Initialize Serial Connection for Debug Logging
   Serial.begin(115200);
+
+  // Start Wifi Autoconfig
   WiFiManager wifiManager;
-  wifiManager.setSTAStaticIPConfig(IPAddress(192,168,1,5), IPAddress(192,168,1,1), IPAddress(255,255,255,0));
+  wifiManager.setSTAStaticIPConfig(IPAddress(192, 168, 1, 5), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
   wifiManager.autoConnect("ESP_CONFIG"); // Will set up as AP if necessary
 
+  ///////////////////////////////////////
+  // Page handlers
+  ///////////////////////////////////////
+
   // Serve control page
-  server.on("/", [](){
+  server.on("/", []() {
     File file = SPIFFS.open("/index.html", "r");
     server.streamFile(file, "text/html");
     file.close();
   });
 
   // Serve API Reference page
-  server.on("/api.html", [](){
+  server.on("/api.html", []() {
     File file = SPIFFS.open("/api.html", "r");
     server.streamFile(file, "text/html");
     file.close();
   });
 
+  // Build and return JSON string with information about each light
   server.on("/lights", []() {
     String response = "[";
     for (int i = 0; i < numLights; i++)
@@ -64,10 +107,10 @@ void setup(void){
                   lights[i]->getDescription() + "\"}"; // comma all but last time
     response += "]";
     server.send(200, "text/plain", response);
-  });
+  }); 
 
-  server.on("/light/set", [](){
-    if(!server.authenticate(www_username, www_password) && authRequired)
+  server.on("/light/set", []() {
+    if (!server.authenticate(www_username, www_password) && authRequired)
       return server.requestAuthentication();
     for (int i = 0; i < numLights; i++)
       if (server.hasArg(String(i)))
@@ -75,34 +118,57 @@ void setup(void){
     server.send(200, "text/plain", "Changes applied");
   });
 
-  server.on("/light/status", [](){
+  server.on("/light/status", []() {
     server.send(200, "text/plain", String(lights[server.arg("id").toInt()]->get()));
   });
 
-  server.on("/settings/doorsensor/enable", [](){
-    if(!server.authenticate(www_username, www_password) && authRequired)
+  server.on("/settings/doorsensor/enable", []() {
+    if (!server.authenticate(www_username, www_password) && authRequired)
       return server.requestAuthentication();
     door_control = true;
-    server.send(200, "text/plain", "enabled");
+    server.send(200, "text/plain", (String)door_control);
   });
 
-  server.on("/settings/doorsensor/disable", [](){
-    if(!server.authenticate(www_username, www_password) && authRequired)
+  server.on("/settings/doorsensor/disable", []() {
+    if (!server.authenticate(www_username, www_password) && authRequired)
       return server.requestAuthentication();
     door_control = false;
-    server.send(200, "text/plain", "disabled");
-  });  
-  
-  server.on("/sensor/brightness", [](){
+    server.send(200, "text/plain", (String)door_control);
+  });
+
+  // Left in for compatibility with old scripts, remove in v2?
+  server.on("/settings/doorsensor", []() {
+    server.send(200, "text/plain", (String)door_control);
+  });
+
+  server.on("/sensor/door", []() {
+    server.send(200, "text/plain", (String)door_control);f
+  });
+
+  server.on("/sensor/brightness", []() {
     server.send(200, "text/plain", (String)analogRead(A0));
   });
 
+  server.on("/sensor/door", []() {
+    server.send(200, "text/plain", (String)digitalRead(door_pin));
+  });
+
+  // Set up server
   server.begin();
-  Serial.println("HTTP server started");
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), setLightState, CHANGE);
+  //Serial.println("HTTP server started");
+
+  // Door handler
+  pinMode(door_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(door_pin), setLightStateOnDoorAction, CHANGE);
+
+  // Manual button handler
+  pinMode(button_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(button_pin), toggleLightOnButtonPress, RISING);
+
+  // Online disable switch
+  pinMode(enable_switch_pin, INPUT_PULLUP);
 }
 
-void loop(void){
+void loop(void) {  
   server.handleClient();
 }
